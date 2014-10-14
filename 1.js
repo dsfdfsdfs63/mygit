0ddaab4e3d22eb5c03e10696cc0f750d7e0de688
dsfdfsdfs63 <dsfdfsdfs63@mail.ru>
2014-10-14 14:34:21 +0000

    second

--- 1.js
+++ 1.js
@@ -1,3 +1,31 @@
-console.log(123);
+var updateUserVersion = function(mgr) {
+    var _self = this;
+    this.manager = mgr;
+    var _models = mgr.models;
 
-// 123
+    this.result = null;
+    this.error = null;
+
+    this.run = function(_user, version) {
+        if (!version || _user.version == version) {
+            _self.emit("complete");
+            return;
+        }
+        var user = {
+            id: _user.id,
+            version: version
+        };
+
+        user = _models.User.using_shard(_user.shard).build(user, {isNewRecord: false});
+        user.save(["version"])
+            .success(function() {
+                _self.emit("complete");
+            })
+            .error(function(error) {
+                _self.error = error;
+                _self.emit("error");
+            });
+    };
+};
+
+module.exports = updateUserVersion;
